184785a225fafd775f4d35d903df54f2fb417983
Araceli Escudero <90579423+araescudero@users.noreply.github.com>
2024-07-23 17:06:20 -0300

    Update EmpleadoFijo.java

--- src/main/java/Ejercicio6/EmpleadoFijo.java
+++ src/main/java/Ejercicio6/EmpleadoFijo.java
@@ -2,10 +2,14 @@ package Ejercicio6;
 
 public class EmpleadoFijo extends Empleado {
 	private double sueldoBasico;
+	private int anioActual = 2024;
+	
 
-    public EmpleadoFijo(String dni, String nombre, String apellido, int anioIngreso, double sueldoBasico) {
+    public EmpleadoFijo(String dni, String nombre, String apellido, int anioIngreso, double sueldoBasico, int anioActual) {
         super(dni, nombre, apellido, anioIngreso);
         this.sueldoBasico = sueldoBasico;
+        this.anioActual = 2024;
+        
     }
 	
 
