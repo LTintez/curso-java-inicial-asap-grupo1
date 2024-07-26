3283477def6b55076ea475adc90d51a26760ef10
Lautaro Tintez <ltintez@asap-consulting.net>
2024-07-26 09:17:09 -0300

    Atributos clases ejercicio 7

---
+++ src/main/java/Ejercicio7/Servicio.java
@@ -0,0 +1,14 @@
+package Ejercicio7;
+
+public class Servicio {
+    private String nombre;
+    private int cantHoras;
+
+    public Servicio(String nombre, int cantHoras){
+        this.nombre = nombre;
+        this.cantHoras = cantHoras;
+    }
+
+    //get precio;
+    //get valorHora;
+}
---
+++ src/main/java/Ejercicio7/ServicioReparacion.java
@@ -0,0 +1,9 @@
+package Ejercicio7;
+
+public class ServicioReparacion extends Servicio{
+    private static final double VALOR_HORA = 500;
+
+    public ServicioReparacion(String nombre, int horas){
+        super(nombre, horas);
+    }
+}
