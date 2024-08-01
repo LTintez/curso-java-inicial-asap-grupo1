e712ca21584c350f71b66bae24c97bb401f4f0df
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-01 10:33:26 -0300

    ExamenDeMoto

--- src/main/java/Ejercicio8/Circuito.java
+++ src/main/java/Ejercicio8/Circuito.java
@@ -3,16 +3,22 @@ package Ejercicio8;
 class Circuito implements Evaluable {
     private static final int NUMERO_FALTAS_PERMITIDAS = 2;
     private int tiempo;
+    private int tiempoEnSeg;
     private int numeroFaltas;
 
     public Circuito(int tiempo, int numeroFaltas) {
         this.tiempo = tiempo;
         this.numeroFaltas = numeroFaltas;
+        this.tiempoEnSeg = tiempoEnSeg;
     }
 
     public int getTiempo() {
         return tiempo;
     }
+
+    public int getTiempoEnSeg(){
+        return tiempoEnSeg;
+    };
 
     public int getNumeroFaltas() {
         return numeroFaltas;
--- src/main/java/Ejercicio8/ExamenDeMoto.java
+++ src/main/java/Ejercicio8/ExamenDeMoto.java
@@ -1,4 +1,43 @@
 package Ejercicio8;
 
-public class ExamenDeMoto {
+public class ExamenDeMoto extends Examen {
+    private static final int TOPE_CILINDRADA = 150;
+    private static final int TIEMPO_MAXIMO_PERMITIDO = 120;
+    private Moto moto;
+
+    public ExamenDeMoto(String fecha, Persona persona, Moto moto){
+        super(fecha, persona);
+        this.moto = moto;
+    }
+
+    @Override
+    public boolean isAprobado() {
+        return false;
+    }
+
+    private int cantCircuitosAprobados(){
+        int count = 0;
+        for (Circuito circuito : circuitos){
+            if (circuito.isAprobado()){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean apruebaExamenMotoAltaCilindrada(){
+        return cantCircuitosAprobados() == circuitos.size();
+    }
+
+    private boolean apruebaExamenMotoBajaCilindrada(){
+        return cantCircuitosAprobados() >= circuitos.size() - 1;
+    }
+
+    private boolean cumpleTiempoTotal() {
+        int totalTiempo = 0;
+        for (Circuito circuito : circuitos) {
+            totalTiempo += circuito.getTiempoEnSeg();
+        }
+        return totalTiempo <= TIEMPO_MAXIMO_PERMITIDO;
+    }
 }
