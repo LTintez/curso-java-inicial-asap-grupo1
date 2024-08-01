a0cad43db4435f39a79798cabb50d0b8e5e1e735
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-01 11:10:27 -0300

    Avanzando con informe

--- src/main/java/Ejercicio8/SedeDeLicencias.java
+++ src/main/java/Ejercicio8/SedeDeLicencias.java
@@ -1,4 +1,50 @@
 package Ejercicio8;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SedeDeLicencias {
+    private List<Examen> examenes;
+
+    public SedeDeLicencias(){
+        this.examenes = new ArrayList<>();
+    }
+
+    public void agregarExamen(Examen examen){
+        this.examenes.add(examen);
+    }
+
+    public String obtenerInforme(){
+        double porcentajeDeReprobados = porcentajeDeReprobados();
+        double promTiempoAutosAprobados = promTiempoAutosAprobados();
+        Informe informe = new Informe(porcentajeDeReprobados, promTiempoAutosAprobados);
+        Gson gson = new Gson();
+        return gson.toJson(informe);
+    }
+
+    private double porcentajeDeReprobados() {
+        int totalExamenes = examenes.size();
+        int examenesReprobados = 0;
+        for (Examen examen : examenes) {
+            if (!examen.isAprobado()) {
+                examenesReprobados++;
+            }
+        }
+        return ((double) examenesReprobados / totalExamenes) * 100;
+    }
+
+    private double promTiempoAutosAprobados() {
+        int totalTiempo = 0;
+        int count = 0;
+        for (Examen examen : examenes) {
+            if (examen instanceof ExamenDeAuto && examen.isAprobado()) {
+                totalTiempo += ((ExamenDeAuto) examen).getTiempoDelCircuito();
+                count++;
+            }
+        }
+        return count > 0 ? (double) totalTiempo / count : 0;
+    }
+
 }
