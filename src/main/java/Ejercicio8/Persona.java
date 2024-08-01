d815c4653ba362bdfbfa7757e21e4ddaa98d5206
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-01 15:52:50 -0300

    Correccion en Persona, agregando Main

--- src/main/java/Ejercicio8/Persona.java
+++ src/main/java/Ejercicio8/Persona.java
@@ -1,13 +1,11 @@
 package Ejercicio8;
 
 public class Persona {
-    private int id;
-    private int dni;
+    private String dni;
     private String nombre;
     private String apellido;
 
-    public Persona(int id, int dni, String nombre, String apellido){
-        this.id = id;
+    public Persona(String dni, String nombre, String apellido){
         this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
